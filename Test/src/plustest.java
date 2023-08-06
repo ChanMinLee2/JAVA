1a62e864ff5831ee88c48ad31ea113df03a90445
chanmin Lee <dlcksals87@gmail.com>
2023-08-06 22:22:05 +0900

    string to integer variable in java

--- Test/src/plustest.java
+++ Test/src/plustest.java
@@ -7,8 +7,18 @@ public class plustest {
         // 입력 방법 1. 정수로 바로 받기
         int num = scanner.nextInt();
 
-        System.out.printf("%d", num);
+        System.out.printf("input number : %d\n", num);
         
+        // 2. 문자열로 받고 정수 전환
+        String strNumber = "42";
+        System.out.printf("number 2 : 42\n");
+        try {
+            // Convert the string to an integer using parseInt()
+            int intValue = Integer.parseInt(strNumber);
+            System.out.println("Converted integer value: " + intValue);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format. Unable to convert the string to an integer.");
+        }
         scanner.close();
     }
     
