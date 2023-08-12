df64208232f6ff2957f983f47e75db06a3ba150c
chanmin Lee <dlcksals87@gmail.com>
2023-08-12 19:02:08 +0900

    quiz 01 - variable and datatype

---
+++ quiz/src/_Quiz_01.java
@@ -0,0 +1,14 @@
+public class _Quiz_01 {
+    public static void main(String[] args) throws Exception {
+        // 버스 도착 정보 출력
+
+        String BusNo = "구로11";
+        int WaitMin = 3;
+        double distance = 1.3;
+
+        System.out.println(BusNo + " 번 버스");
+        System.out.println("약 " + WaitMin + " 분 남았습니다.");
+        System.out.println("남은 거리 " + distance + " km ");
+
+    }
+}
