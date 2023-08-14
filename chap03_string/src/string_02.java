4966a8d68b9af01cbb306b71f813540b4658499f
chanmin Lee <dlcksals87@gmail.com>
2023-08-15 00:45:39 +0900

    string 2 - replace, substring

---
+++ chap03_string/src/string_02.java
@@ -0,0 +1,11 @@
+public class string_02 {
+    public static void main(String[] args) {
+        String s = "I like Java and Python and C.";
+
+        // 문자열 변환
+        System.out.println(s.replace("and",",")); // and를 ,로 변환해서 출력
+        System.out.println(s.substring(7)); //인덱스 기준 7부터 시작 (이전 내용 삭제)
+
+
+    }
+}
