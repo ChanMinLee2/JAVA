0e20fcd45516c8522482fae1a6e1eb589fea0663
chanmin Lee <dlcksals87@gmail.com>
2023-08-17 22:12:32 +0900

    string 4 - escape sequence

---
+++ chap03_string/src/string_04.java
@@ -0,0 +1,21 @@
+public class string_04 {
+    public static void main(String[] args) {
+        System.out.println("자바가");
+        System.out.println("너무");
+        System.out.println("재밌어요");
+
+        // \n 줄바꿈
+        System.out.println("자바가 \n 너무 \n 재밌어요");
+
+        // \t 탭 문자 삽입
+        System.out.println("해물파전\t9000원");
+        System.out.println("김치전\t8000원");
+        System.out.println("부추전\t8000원");
+
+        // \\ 역슬래시
+        System.out.println("C:\\program Files\\java");
+
+        // \" 큰 따옴표 (작은따옴표 동일)
+        System.out.println("단비가 \"냐옹\" 이라고 했어요");
+    }
+}
