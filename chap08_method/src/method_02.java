995a137d347a307a9a1ce8c64e386f339478517a
chanmin Lee <dlcksals87@gmail.com>
2023-09-04 22:30:35 +0900

    parameter in method

---
+++ chap08_method/src/method_02.java
@@ -0,0 +1,13 @@
+public class method_02 {
+    public static void power(int number)
+    {
+        int result = number *number;
+        System.out.println(number + " 의 2 승은 " + result);
+    }
+    public static void main(String[] args) {
+        // 전달값 (parameter)
+
+        power(2); // 자바에서는 변수 자리에 어떤 변수명에 전달하는지 적어줌. 
+    }
+    
+}
