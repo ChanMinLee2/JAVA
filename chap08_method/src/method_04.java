237fd079bbb412932b7e899e5c25379d25859f75
chanmin Lee <dlcksals87@gmail.com>
2023-09-05 23:27:23 +0900

    method 4 return and parameter

---
+++ chap08_method/src/method_04.java
@@ -0,0 +1,27 @@
+public class method_04 {
+    public static int getPower(int number){
+        int result = number * number;
+        return result;
+    }
+
+    public static int getPowerByExp(int number, int exponent){
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= number;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int retVal = getPower(2);
+        System.out.println(retVal);
+
+        retVal = getPower(3);
+        System.out.println(retVal);
+
+        retVal = getPowerByExp(3, 3);
+        System.out.println(retVal);
+
+        System.out.println(getPowerByExp(2, 4));
+    }
+}
